640032e28d87f27276bbbafbb3b3b5e74cf8c936
David Soto <urielsonic@gmail.com>
2018-02-21 08:45:37 -0600

    Laboratorio 6 terminados los requisitos

---
+++ Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+//David Soto y Kevin Macario
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler {
+    private VirtualButtonBehaviour virtualBtn;
+    public Animator birdAnim;
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+        birdAnim.SetTrigger("sing");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+        virtualBtn = GetComponent<VirtualButtonBehaviour>();
+        virtualBtn.RegisterEventHandler(this);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+}
